62a4121ab92b5ef1c7ee9081d9c01b5ffad9fae0
Virantha Ekanayake <virantha@gmail.com>
2020-07-23 14:36:47 -0400

    added ability to run tesseract in aws

--- lexic/command.py
+++ lexic/command.py
@@ -2,8 +2,17 @@
 """
     Base class to represent all the external commands we have to invoke
 """
-import platform, logging, shutil, os, sys
+import platform, logging, shutil, os, sys, io, base64
+from requests import Request, Session
+import requests
+from tenacity import retry, wait_random, wait_fixed, stop_after_attempt, wait_random_exponential, stop_after_delay
 from curio import subprocess, spawn, Queue
+from curio.file import aopen
+import curio, asks
+asks.init('curio')
+
+import boto3
+
 from tqdm import tqdm
 import yaml
 
@@ -11,6 +20,12 @@ logger = logging.getLogger(__name__)
 
 from .exc import UnsupportedOSError, UnknownExecutableError
 from .item import ItemList
+
+session = Session()
+session_adapter = requests.adapters.HTTPAdapter(pool_connections=20, pool_maxsize=100)
+session.mount('https://', session_adapter)
+
+asks_session = asks.Session(connections=100)
 
 class Cmd:
 
@@ -96,20 +111,72 @@ class Cmd:
         logger.debug(cmd)
         output = await subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
         return output
+    
+    async def _get_aws_signed_url(self, url):
+        response = await asks_session.get(url, retries=3)
+        response.raise_for_status()
+        return response.json()
 
-    async def run_in_parallel(self, tasks):
-        t_list = []  # All currently executing tasks
-        for task in self.iterate_with_progress(tasks):
-            if not self.skip:
-                t = await spawn(task[0], *task[1:])  
-                t_list.append(t)
-                if len(t_list) == self.N_THREADS:
-                    for t in t_list:
-                        await t.join()
-                    t_list = []
-        if not self.skip:
-            for t in t_list:
-                await t.join()
+    def _aws_post_file(self, filename, signed_response):
+        # Not sure why asks isn't working here
+        upload_url = signed_response['url']
+        upload_data = signed_response['fields']
+        upload_filename = upload_data['key']
+        with open(filename, 'rb') as f:
+            files = {'file': (upload_filename, f)}
+            http_response = requests.post(upload_url, data=upload_data, files=files)
+            #http_response = await asks_session.post(upload_url, data=upload_data, files=files)
+            http_response.raise_for_status()
+
+    @retry(sleep=curio.sleep, wait=wait_random_exponential(multiplier=1, max=60), stop=stop_after_delay(10))
+    async def _aws_upload_file(self, url, filename):
+        # Get signed URL for AWS S3
+        # POst to signed URL
+        #url_response = await curio.run_in_thread(self.get_aws_signed_url, url)
+        url_response = await self._get_aws_signed_url(url)
+
+        # Not sure why i can't use asks here to post the files
+        response = await curio.run_in_thread(self._aws_post_file, filename, url_response)
+        #response = await self._aws_post_file(filename, url_response)
+        return url_response
+
+    @retry(sleep=curio.sleep, wait=wait_random_exponential(multiplier=1, max=60), stop=stop_after_delay(10))
+    async def _aws_run_command(self, url, input_files, cmd, output_filenames):
+        """Post to aws to run the specified commands with input files, and return
+           the output files
+        """
+        json = {
+            'input_files': input_files,
+            'cmd': cmd,
+            'output_files': [os.path.basename(fn) for fn in output_filenames],
+        }
+        response = await asks_session.post(url, json=json)
+        response.raise_for_status()
+        return response.json()
+
+    async def run_command_aws(self, cmd, input_filenames, output_filenames, url):
+        logger.debug(f'Running in aws: {cmd}')
+        endpoint_run_ocr = f'{url}/ocr'
+        endpoint_get_signed_url = f'{url}/geturl'
+
+        # First, upload the input_files
+        input_files = []
+        for input_filename in input_filenames:
+            logger.debug(f'Uploading file {input_filename} to S3')
+            url_response = await self._aws_upload_file(endpoint_get_signed_url, input_filename)
+            input_files.append((url_response['fields']['key'], os.path.basename(input_filename)))
+
+        logging.info(f'Posting to aws {cmd}')
+        response_dict = await self._aws_run_command(endpoint_run_ocr, input_files, cmd, output_filenames)
+        logging.debug (response_dict['message'])
+
+        #print (list(response_dict['output_files'].keys()))
+        for output_filename in output_filenames:
+            contents = response_dict['output_files'][os.path.basename(output_filename)]
+            b64 = base64.b64decode(contents)
+            async with aopen(str(output_filename), 'wb') as f:
+                await f.write(b64)
+
             
     async def add_to_queue(self, output_filename, task_func, *task_args):
         self.queue[output_filename] = (task_func, *task_args)
@@ -127,22 +194,33 @@ class Cmd:
             await Cmd.msg_queue.task_done()
         return msgs
 
+    async def spawn_with_update(self, pbar, task):
+        await task[0](*task[1:]) 
+        pbar.update(1)
+
     async def run_queue(self):
         t_list = []  # All currently executing tasks
         output_filenames = []
-        for output_filename, task in self.iterate_with_progress(self.queue.items()):
-            output_filenames.append(output_filename)
+        n = len(self.queue)
+        if self.skip:
+            desc = f'{self.name} [skipped]'
+        else:
+            desc = self.name
+
+        with tqdm(total=n, desc=desc) as pbar:
+            for output_filename, task in self.queue.items():
+                output_filenames.append(output_filename)
+                if not self.skip:
+                    t = await spawn(self.spawn_with_update, pbar, task)
+                    t_list.append(t)
+                    if len(t_list) == self.N_THREADS:
+                        for t in t_list:
+                            await t.join()
+                        t_list = []
             if not self.skip:
-                t = await spawn(task[0], *task[1:])  
-                t_list.append(t)
-                if len(t_list) == self.N_THREADS:
-                    for t in t_list:
-                        await t.join()
-                    t_list = []
-        if not self.skip:
-            # Flush out any remaining jobs in the queue (when N_THREADS is not an int factor of the task count)
-            for t in t_list:
-                await t.join()
+                # Flush out any remaining jobs in the queue (when N_THREADS is not an int factor of the task count)
+                for t in t_list:
+                    await t.join()
         self.queue = {}  # Empty all the jobs
         return ItemList([os.path.abspath(p) for p in output_filenames])
 
--- lexic/plugins/ocr_tesseract.py
+++ lexic/plugins/ocr_tesseract.py
@@ -1,4 +1,6 @@
-import logging, os, subprocess
+import logging, os, subprocess, traceback
+
+from tenacity import retry
 
 from ..command import Cmd
 from ..item import ItemList
@@ -16,22 +18,48 @@ class Plugin(Cmd):
     stage = 'ocr'
     inputs_from = ['image']
     
-    options = ["--ocr_tesseract-program=PROGRAM    path to Tesseract program [default: tesseract]"]
+    options = ["--ocr_tesseract-program=PROGRAM    path to Tesseract program [default: tesseract]",
+               "--ocr_tesseract-aws                run in cloud",
+               "--ocr_tesseract-awsurl=URL         endpoint to use for aws"
+    ]
 
     async def run(self, item_list):
         logger.info(f'About tesseract {item_list}')
 
         with item_list as items:
+            n_items = len(items)
+            old_n_threads = self.N_THREADS
+            if self.config['aws']:
+                self.N_THREADS=n_items
+                assert self.config['awsurl'] is not None, 'URL for AWS services not specified'
+
             for item in items:
                 basename = self._get_filename_base(item)
                 tsv_filename = self._change_ext(item, 'tsv')
-                await self.add_to_queue(tsv_filename, self.call_tesseract, item, basename)
+                if self.config['aws']:
+                    run_method = self.call_tesseract_aws
+                else:
+                    run_method = self.call_tesseract
+                await self.add_to_queue(tsv_filename, run_method, item, basename)
 
-            return await self.run_queue()
-    
+            out_filenames = await self.run_queue()
+            self.N_THREADS = old_n_threads
+            
+            return out_filenames
+
     async def call_tesseract(self, item, basename):
         try:
             cmd = f'{self.executable} {item} {basename} --psm 1 tsv'
             await self._run_command(cmd)
         except (subprocess.CalledProcessError, IOError):
             self.error("Tesseract OCR could not be executed")
+
+    async def call_tesseract_aws(self, item, basename):
+        try:
+            #print(f"Calling aws! {item}")
+            cmd = f'{self.executable} {os.path.basename(item)} {basename} --psm 1 tsv'
+            await self.run_command_aws(cmd, [item], [f'{basename}.tsv'], self.config['awsurl'] )
+        except Exception as e:
+            print(str(e))
+            print(f"ERROR: Tesseract OCR could not be executed on {item}")
+            traceback.print_exc()
